d8dfef46cd3fc608d70ac9fbe7befa51556cfbd4
Youssif24 <youssif.saad96@gmail.com>
2018-12-04 01:40:10 +0200

    edit login code

--- app/src/main/java/com/saad/youssif/arabiclawyer/LoginActivity.java
+++ app/src/main/java/com/saad/youssif/arabiclawyer/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.saad.youssif.arabiclawyer;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +19,8 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     Button enterBtn;
     EditText nameEt,passwordEt;
     ProgressDialog progressDialog;
-    //this is test comment
+    ConnectivityManager connectivityManager;
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +31,40 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         passwordEt=findViewById(R.id.login_passwordEt);
         progressDialog=new ProgressDialog(this);
         lawyerPresenter=new LawyerPresenter(this,this);
+        sharedPrefManager=SharedPrefManager.getInstance(this);
+        connectivityManager= (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        register_from_shrd();
         enterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressDialog.setMessage("جاري تسجيل الدخول........");
-                progressDialog.show();
-                lawyerPresenter.lawyerLogin(nameEt.getText().toString(),passwordEt.getText().toString());
+                if(checkConnection())
+                {
+                    if(validateData())
+                    {
+                        progressDialog.setMessage("جاري تسجيل الدخول........");
+                        progressDialog.show();
+                        lawyerPresenter.lawyerLogin(nameEt.getText().toString(),passwordEt.getText().toString());
+                    }
+
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this,"من فضلك تأكد من إتصالك بالانترنت....",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
 
     @Override
     public void showLoginResult(String result) {
-        Toast.makeText(this,result,Toast.LENGTH_LONG).show();
         if(result.equals("success"))
         {
             Toast.makeText(this,"تم تسجيل الدخول بنجاح",Toast.LENGTH_LONG).show();
+            save_user_data(nameEt.getText().toString(),passwordEt.getText().toString());
             progressDialog.dismiss();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            LoginActivity.this.finish();
         }
         else
         {
@@ -49,5 +72,46 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
             progressDialog.dismiss();
         }
 
+    }
+
+    public boolean checkConnection()
+    {
+        NetworkInfo info=connectivityManager.getActiveNetworkInfo();
+        return info!=null&&info.isConnected();
+    }
+
+    public boolean validateData()
+    {
+        if(nameEt.getText().toString().trim().equals(""))
+        {
+            nameEt.setError("إدخل الإسم");
+            return false;
+        }
+        else if(passwordEt.getText().toString().trim().equals(""))
+        {
+            passwordEt.setError("إدخل كلمة المرور");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+    public void save_user_data(String username,String password)
+    {
+        sharedPrefManager.setName(username.trim());
+        sharedPrefManager.setPassword(password.trim());
+    }
+    public void register_from_shrd()
+    {
+        String name=sharedPrefManager.getName();
+        String pass=sharedPrefManager.getPassword();
+        if(!(TextUtils.isEmpty(name))&&!(TextUtils.isEmpty(pass)))
+        {
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            LoginActivity.this.finish();
+        }
     }
 }
---
+++ app/src/main/java/com/saad/youssif/arabiclawyer/SharedPrefManager.java
@@ -0,0 +1,64 @@
+package com.saad.youssif.arabiclawyer;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefManager {
+    private static final String SHARED_PREF_NAME = "LawyerLogin";
+    private static final String TAG_NAME = "username";
+    private static final String TAG_PASSWORD = "password";
+    private String name,id,password;
+
+
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    public String getName() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return  sharedPreferences.getString(TAG_NAME, null);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TAG_NAME,name);
+        editor.apply();
+    }
+
+
+
+    public String getPassword() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return  sharedPreferences.getString(TAG_PASSWORD, null);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TAG_PASSWORD, password);
+        editor.apply();
+    }
+
+    public void clearSharedData()
+    {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear().commit();
+
+    }
+
+
+}
